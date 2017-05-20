318e2e0b2896802a73deed12c3916f02bea4226b
moshe-arad <moshe.arad.mailbox@gmail.com>
2017-05-20 22:14:30 +0300

    refacroring front + in particular the angularjs code - home

--- src/main/java/org/moshe/arad/controllers/UsersController.java
+++ src/main/java/org/moshe/arad/controllers/UsersController.java
@@ -3,14 +3,11 @@ package org.moshe.arad.controllers;
 import java.io.IOException;
 import java.util.Arrays;
 
-import javax.validation.Valid;
-
 import org.moshe.arad.entities.BackgammonUser;
 import org.moshe.arad.entities.Status;
 import org.moshe.arad.replies.IsUserFoundReply;
 import org.moshe.arad.requests.UserCredentials;
 import org.moshe.arad.services.HomeService;
-import org.moshe.arad.validators.BackgammonUserValidator;
 import org.moshe.arad.websocket.EmailAvailabilityMessage;
 import org.moshe.arad.websocket.UserNameAvailabilityMessage;
 import org.slf4j.Logger;
@@ -20,14 +17,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -77,24 +70,7 @@ public class UsersController {
 	}
 	
 	@RequestMapping(value = "/users/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public ResponseEntity<String> createNewUser(@Valid @RequestBody BackgammonUser backgammonUser, Errors errors){
-		if(errors.hasErrors()){
-			logger.info("Some errors occured while trying to bind backgammon user");
-			logger.info("There are " + errors.getErrorCount() + " errors.");
-			
-			for(FieldError error:errors.getFieldErrors()){
-				logger.warn("Field name:  " + error.getField());
-				logger.warn(error.getDefaultMessage());
-			}
-			
-			if(!BackgammonUserValidator.acceptableErrors(errors)){
-				logger.info("Routing for home page.");
-				HttpHeaders header = new HttpHeaders();
-				header.add("Content-Type", "application/json");
-				return new ResponseEntity<String>(header, HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		}
-		
+	public ResponseEntity<String> createNewUser(@RequestBody BackgammonUser backgammonUser, Errors errors){
 		logger.info("The GameUser bind result: " + backgammonUser);
 		
 		try{
@@ -162,10 +138,5 @@ public class UsersController {
 			logger.error(ex.toString());
 			return new EmailAvailabilityMessage(false);
 		}
-	}
-	
-	@InitBinder
-	public void initBinder(WebDataBinder binder){
-		binder.addValidators(new BackgammonUserValidator());
 	}
 }
